3516c006bf74b226f9e1c340cab70815ed59df9f
SoftwrDev <contato.santosgabriel@gmail.com>
2021-02-13 15:03:15 -0300

    feat: use print subroutine instead of printing directly

--- hello_world/hello.asm
+++ hello_world/hello.asm
@@ -1,23 +1,31 @@
 section .data
-	text db "Hello, World!",0xa
+	text db "Hello, World!",0xa,0
 
 section .text
 	global _start
 
 _start:
-	call _printHello
-	call _exit
+	mov rax, text
+	call _print
 
-_printHello:
-	mov rax, 1    ; sys_write
-	mov rdi, 1    ; std_out
-	mov rsi, text ; data
-	mov rdx, 14   ; size of data
-	syscall
-
-	ret
-
-_exit:
 	mov rax, 60   ; sys_exit
 	mov rdi, 0    ; error code
 	syscall
+
+_print:
+    push rax       ; pushes rax into the stack 
+    mov rbx, 0     ; loads rbx as counter
+_printLoop:
+    inc rax        ; increments the pointer in rax
+    inc rbx        ; increments the counter
+    mov cl, [rax]  ; moves the value of char* to cl register
+    cmp cl, 0      ; checks wheter or not cl is equal to 0
+    jne _printLoop ; while not true, calls again _printLoop
+
+    mov rax, 1     ; SYS_WRITE
+    mov rdi, 1     ; STDOUT
+    pop rsi        ; pops the rax pointer from the stack
+    mov rdx, rbx   ; loads the counter for the size of the actual string
+    syscall
+
+    ret
