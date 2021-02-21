c822f7a28441daf7d9158b4a1ce8e36304011918
SoftwrDev <contato.santosgabriel@gmail.com>
2021-02-20 23:49:06 -0300

    feat: add comentary to calculate_string x86

--- x86/003_calculate_string/calculate_string.asm
+++ x86/003_calculate_string/calculate_string.asm
@@ -6,14 +6,14 @@ _start:
 	mov eax, ebx
 
 nextchar:
-	cmp byte[eax], 0
-	jz exit
-	inc eax
-	jmp nextchar
+	cmp byte[eax], 0 ; checks null character
+	jz exit		 ; if null jumps to exit
+	inc eax		 ; else increments the eax register
+	jmp nextchar	 ; redo the process
 exit:
-	sub eax, ebx
+	sub eax, ebx	 ; gets the difference between the starting address and the end
 
-	mov edx, eax ; size of string
+	mov edx, eax 	 ; now we have the size of string
 	
 	mov eax, 4
 	mov ebx, 1
