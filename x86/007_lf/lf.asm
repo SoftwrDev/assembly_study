4be7d877349f263bf59ec35e92d2ea75aca25ed3
SoftwrDev <contato.santosgabriel@gmail.com>
2021-02-21 16:32:46 -0300

    feat: add lf program

---
+++ x86/007_lf/lf.asm
@@ -0,0 +1,17 @@
+%include "functions.asm"
+
+section .text
+	global _start
+
+_start:
+	mov eax, msg1
+	call sprintLF
+
+	mov eax, msg2
+	call sprintLF
+
+	call quit
+
+section .data
+	msg1: db "Hello, brave new world!", 0h
+	msg2: db "This is how we recycle in NASM.", 0h
